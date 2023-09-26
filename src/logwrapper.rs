5f61af43958065e07f04b53c4b522861ff318ed7
chordtoll <git@chordtoll.com>
2023-09-26 00:40:19 -0700

    Add tests for log wrapper

--- src/logwrapper.rs
+++ src/logwrapper.rs
@@ -22,3 +22,28 @@ impl<T: LogWrapper, E: LogWrapper> LogWrapper for Result<T, E> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::logwrapper::LogWrapper;
+
+    #[test]
+    fn test_lw_int() {
+        assert_eq!(5i32.lw(), String::from("5"))
+    }
+
+    #[test]
+    fn test_lw_bytes() {
+        assert_eq!(vec![1u8, 2u8, 3u8, 4u8, 5u8].lw(), String::from("[5]"))
+    }
+
+    #[test]
+    fn test_lw_ok() {
+        assert_eq!(Ok::<i32, i32>(3).lw(), String::from("Ok(3)"))
+    }
+
+    #[test]
+    fn test_lw_err() {
+        assert_eq!(Err::<i32, i32>(3).lw(), String::from("Err(3)"))
+    }
+}
