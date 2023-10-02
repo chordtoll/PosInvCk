d70b542e086b3d99631ce6b3aa01b8ea3d989a11
chordtoll <git@chordtoll.com>
2023-10-01 22:51:21 -0700

    Add test: rename

--- src/fs/rename.rs
+++ src/fs/rename.rs
@@ -4,6 +4,7 @@ use crate::{
     fs::{chdirin, chdirout, restore_ids, set_ids},
     invariants::fs::rename::{inv_rename_after, inv_rename_before},
     log_call, log_more, log_res,
+    req_rep::{ReplyEmpty, Request},
 };
 
 use super::InvFS;
@@ -11,13 +12,13 @@ use super::InvFS;
 impl InvFS {
     pub fn do_rename(
         &mut self,
-        req: &fuser::Request<'_>,
+        req: Request,
         parent: u64,
         name: &std::ffi::OsStr,
         newparent: u64,
         newname: &std::ffi::OsStr,
         flags: u32,
-        reply: fuser::ReplyEmpty,
+        reply: &ReplyEmpty,
     ) {
         let callid = log_call!(
             "RENAME",
@@ -31,9 +32,9 @@ impl InvFS {
         let cwd = chdirin(&self.root);
         let mut dl = self.data.lock().unwrap();
         let inv = inv_rename_before(
-            callid, req, &self.root, parent, name, newparent, newname, flags, &mut dl,
+            callid, &req, &self.root, parent, name, newparent, newname, flags, &mut dl,
         );
-        let ids = set_ids(callid, req.into(), None);
+        let ids = set_ids(callid, req, None);
         let ip = &mut dl.INODE_PATHS;
         let old_parent = ip.get(parent);
         log_more!(callid, "old_parent={:?}", old_parent);
@@ -64,5 +65,105 @@ impl InvFS {
             }
             Err(v) => reply.error(v),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::ffi::OsString;
+
+    use crate::{
+        fs::TTL,
+        req_rep::{KernelConfig, ReplyCreate, ReplyEmpty, ReplyEntry, Request},
+    };
+
+    #[test]
+    fn test_rename() {
+        let mut ifs = crate::test::create_ifs();
+        ifs.do_init(
+            Request {
+                uid: 0,
+                gid: 0,
+                pid: 0,
+            },
+            &KernelConfig::empty(),
+        )
+        .unwrap();
+        let rep_c = ReplyCreate::new();
+        ifs.do_create(
+            Request {
+                uid: 0,
+                gid: 0,
+                pid: 0,
+            },
+            1,
+            &OsString::from("foo"),
+            0,
+            0,
+            libc::O_CREAT,
+            &rep_c,
+        );
+        let rep_r = ReplyEmpty::new();
+        ifs.do_rename(
+            Request {
+                uid: 0,
+                gid: 0,
+                pid: 0,
+            },
+            1,
+            &OsString::from("foo"),
+            1,
+            &OsString::from("bar"),
+            0,
+            &rep_r,
+        );
+        assert_eq!(rep_r.get(),Ok(()));
+        let rep_old = ReplyEntry::new();
+        ifs.do_lookup(
+            Request {
+                uid: 0,
+                gid: 0,
+                pid: 0,
+            },
+            1,
+            &OsString::from("foo"),
+            &rep_old,
+        );
+        let rep_new = ReplyEntry::new();
+        ifs.do_lookup(
+            Request {
+                uid: 0,
+                gid: 0,
+                pid: 0,
+            },
+            1,
+            &OsString::from("bar"),
+            &rep_new,
+        );
+        assert_eq!(rep_old.get(), Err(libc::ENOENT));
+        assert_eq!(
+            rep_new.get(),
+            Ok((
+                TTL,
+                fuser::FileAttr {
+                    ino: rep_c.get().unwrap().1.ino,
+                    size: 0,
+                    blocks: 0,
+                    atime: rep_c.get().unwrap().1.atime,
+                    mtime: rep_c.get().unwrap().1.mtime,
+                    ctime: rep_c.get().unwrap().1.ctime,
+                    crtime: rep_c.get().unwrap().1.crtime,
+                    kind: fuser::FileType::RegularFile,
+                    perm: 0,
+                    nlink: 1,
+                    uid: 0,
+                    gid: 0,
+                    rdev: 0,
+                    blksize: 4096,
+                    flags: 0
+                },
+                0
+            ))
+        )
     }
 }
